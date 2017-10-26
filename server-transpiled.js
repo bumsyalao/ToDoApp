562e01b48a9904b32b9847f16674bd4caa1c5338
bumsyalao <alaobunmi93@gmail.com>
2017-10-26 13:15:39 +0100

    chore: setup babel-preset
    - install dependencies
    - specify babel preset
    - add start script
    - create directories
    
    [Delivers #1]

---
+++ server-transpiled.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log("bfuywegcubc");
